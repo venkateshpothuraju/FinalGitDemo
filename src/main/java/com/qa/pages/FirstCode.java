dfab66d417e3b5fb3a14977288e161ea3e4b03ef
venkateshpothuraju <venkateshpothuraju94@outlook.com>
2021-12-28 22:19:07 +0530

    FIrst Commit

---
+++ src/main/java/com/qa/pages/FirstCode.java
@@ -0,0 +1,12 @@
+package com.qa.pages;
+
+public class FirstCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Virat Kohli");
+		System.out.println("Rohith sharma");
+
+	}
+
+}
---
+++ src/main/java/com/qa/pages/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.qa.pages;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helo");
+		System.out.println("world");
+
+	}
+
+}
